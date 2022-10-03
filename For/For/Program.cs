e0df53d239066cbc0f3c0b76e382c85213160ea7
xXWEEBMASTERXx <74237896+xXWEEBMASTERXx@users.noreply.github.com>
2022-10-03 14:26:15 +0200

    C Octothorpe 3
    2022. 10. 03.

--- For/For/Program.cs
+++ For/For/Program.cs
@@ -10,11 +10,11 @@ namespace For
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
             /*
             // 1. Feladat
             Console.Write("Adj meg egy számot");
             int num = int.Parse(Console.ReadLine());
-            
             for (int i = 0; i < num; i++)
             {
                 Console.WriteLine(i);
@@ -40,7 +40,7 @@ namespace For
             {
                 Console.WriteLine(i);
             }
-            Console.ReadLine();*/
+            Console.ReadLine();
 
             // 5. Feladat
             Console.Write("Adj meg egy számot");
@@ -52,6 +52,158 @@ namespace For
                 Console.WriteLine(i);
             }
             Console.ReadLine();
+
+            //6. Feladat
+            Console.Write("Adj meg egy számot");
+            int num4 = int.Parse(Console.ReadLine());
+            for (int i = num4; i < 0; i--)
+            {
+                int x = num4;
+                int y = 2;
+                int z = num4 * y;
+                Console.Write(z);
+                y++;
+            }
+            Console.ReadLine();
+
+            //7.feladat
+            Console.Write("Adj meg egy számot");
+            int num5 = int.Parse(Console.ReadLine());
+            Console.Write("Adj meg egy nagyobb számot");
+            int num6 = int.Parse(Console.ReadLine());
+            for (int i = num5; i < num6; i++)
+            {
+                if (i++ < num6)
+                {
+                    Console.WriteLine(i++);
+                }
+                else
+                    break;
+            }
+            Console.ReadLine();
+            
+            //8. Feladat
+            Console.Write("Adj meg egy számot");
+            int num7 = int.Parse(Console.ReadLine());
+            for (int i = 0; i < num7; i++)
+            {
+                if ((i/num7)%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+            
+            //9. Feladat
+            Console.Write("Adj meg egy számot");
+            double num8 = int.Parse(Console.ReadLine());
+            Console.Write("Adj meg a szám kitevőjét");
+            double num9 = int.Parse(Console.ReadLine());
+            double hatvany = Math.Pow(num8,num9);
+            Console.WriteLine(hatvany);
+            Console.ReadLine();
+            
+            //10. Feladat
+            Console.Write("Adj meg egy számot");
+            int num10 = int.Parse(Console.ReadLine());
+            Console.Write("Adj meg egy számot");
+            int num11 = int.Parse(Console.ReadLine());
+            for (int i = num10; i < num11; i++)
+            {
+                if ((i / num11) % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+
+            //11. Feladat
+            Console.Write("Adj meg egy számot");
+            int num12 = int.Parse(Console.ReadLine());
+            for (int i = num12; i > 0; i--)
+            {
+                int randnum = random.Next(0, 100);
+                Console.WriteLine(randnum);
+            }
+            Console.ReadLine();
+            
+            //12. Feladat
+            Console.Write("Add meg a véletlen számok darabját");
+            int num13 = int.Parse(Console.ReadLine());
+            Console.Write("Adj meg egy számot");
+            int num14 = int.Parse(Console.ReadLine());
+            Console.Write("Adj meg egy nagyobb számot");
+            int num15 = int.Parse(Console.ReadLine());
+            for (int i = num13; i > 0; i--)
+            {
+                double randnum2 = (random.NextDouble() * (num15 - num14) +num15);
+                Console.WriteLine(randnum2);
+            }
+            Console.ReadLine();
+            
+            //13. Feladat
+            Console.Write("Adj meg egy számot");
+            int num15 = int.Parse(Console.ReadLine());
+            for (int i = num15; i > 0; i--)
+            {
+                int randnum3 = random.Next(1,3);
+                Console.WriteLine(randnum3);
+            }
+            Console.ReadLine();
+
+            //14. Feladat
+            Console.Write("Adj meg egy számot");
+            int num16 = int.Parse(Console.ReadLine());
+            for (int i = 0; i > num16; i--)
+            {
+                int x = 1;
+                int y = x;
+                int z = y * x++;
+                if (i == num16)
+                {
+                    Console.WriteLine(z);
+                }
+            }
+            Console.ReadLine();
+
+            //15. Feladat
+            Console.Write("Adj meg egy számot");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < width; i++)
+            {
+                Console.Write("#");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < width - 2; i++)
+            {
+                Console.Write("#");
+
+                for (int j = 0; j < width - 2; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine("#");
+            }
+
+            for (int i = 0; i < width; i++)
+            {
+                Console.Write("#");
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+
+            //16. Feladat
+            for (int i = 0; i < 1; i--)
+            {
+                Console.WriteLine("XOXOXO");
+                Console.WriteLine("OXOXOX");
+            }
+            Console.ReadLine();*/
         }
     }
 }
