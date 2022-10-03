eced1b580272b28f0390abf0d8f2ded9e6e91a01
xXWEEBMASTERXx <74237896+xXWEEBMASTERXx@users.noreply.github.com>
2022-10-03 15:07:52 +0200

    C Octothorpe 3
    2022. 10. 03.

---
+++ Tekszt/Tekszt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tekszt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Feladat
+            //a)
+            Console.WriteLine("adj meg egy szöveget: ");
+            string hossz = Console.ReadLine();
+            int stringLenght = hossz.Length;
+            Console.WriteLine(stringLenght);
+            Console.ReadLine();
+            
+            //b)
+            for (int i = hossz.Length-1; i >= 0; i--)
+            {
+                Console.WriteLine(hossz[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
