e9f253c656874b065ef787a969ff633b6ba38df1
xXWEEBMASTERXx <74237896+xXWEEBMASTERXx@users.noreply.github.com>
2022-10-10 15:02:44 +0200

    C Octothorpe 4
    2022. 10. 10.

--- Tekszt/Tekszt/Program.cs
+++ Tekszt/Tekszt/Program.cs
@@ -13,17 +13,72 @@ namespace Tekszt
             //1. Feladat
             //a)
             Console.WriteLine("adj meg egy szöveget: ");
-            string hossz = Console.ReadLine();
-            int stringLenght = hossz.Length;
+            string szoveg = Console.ReadLine();
+            int stringLenght = szoveg.Length;
             Console.WriteLine(stringLenght);
             Console.ReadLine();
-            
+
             //b)
-            for (int i = hossz.Length-1; i >= 0; i--)
+            for (int i = 0; i <= szoveg.Length - 1; i++)
             {
-                Console.WriteLine(hossz[i]);
+                Console.WriteLine(szoveg[i]);
             }
             Console.ReadLine();
+            
+            //c)
+            for (int i = szoveg.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(szoveg[i]);
+            }
+            Console.ReadLine();
+            
+            //d)
+            string nagybetu = szoveg.ToUpper();
+            Console.WriteLine();
+
+            //e)
+            string kisbetu = szoveg.ToLower();
+            Console.WriteLine();
+
+            //f)
+            // idfk
+
+            //2. Feladat
+            //a)
+            Console.WriteLine("adj meg egy szöveget: ");
+            string szoveg2 = Console.ReadLine();
+            Console.WriteLine("adj meg még egy szöveget: ");
+            string szoveg3 = Console.ReadLine();
+            string ossze = string.Format(szoveg2+szoveg3);
+            Console.WriteLine(ossze);
+            Console.ReadLine();
+
+            //b)
+            int hossz2 = szoveg2.Length;
+            int hossz3 = szoveg3.Length;
+            if (hossz2 > hossz3)
+            {
+                Console.WriteLine("az 1. a hosszabb. https://i.kym-cdn.com/entries/icons/original/000/037/848/cover2.jpg");
+                Console.ReadLine();
+            }
+            else if (hossz3 > hossz2)
+            {
+                Console.WriteLine("a 2. a hosszabb. https://i.kym-cdn.com/entries/icons/original/000/037/848/cover2.jpg");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("a 2 szöveg egyenlő hoszú. https://i.kym-cdn.com/entries/icons/original/000/037/848/cover2.jpg");
+                Console.ReadLine();
+            }
+
+            //c)
+            string ossze2 = string.Format(szoveg2+szoveg3);
+            Console.WriteLine(ossze2);
+            Console.ReadLine();
+
+            //d)
+
         }
     }
 }
