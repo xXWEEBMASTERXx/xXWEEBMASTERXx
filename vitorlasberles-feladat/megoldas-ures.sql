a8734fdbd197bf7120af2b6a9834ea300db911ad
xXWEEBMASTERXx <74237896+xXWEEBMASTERXx@users.noreply.github.com>
2022-11-21 12:47:52 +0100

    MySQL 2022. 11. 21.

--- vitorlasberles-feladat/megoldas-ures.sql
+++ vitorlasberles-feladat/megoldas-ures.sql
@@ -1,15 +1,25 @@
 -- 3. feladat
-
+SELECT DISTINCT `tipus`
+FROM `hajo`;
 -- 4. feladat
-
+SELECT MIN(`dij`) AS 'Minimum'
+FROM `hajo`;
 -- 5. feladat
-
+SELECT `nev`, `utas`, `dij`
+FROM `hajo`
+ORDER BY `utas` DESC
+LIMIT 3;
 -- 6. feladat
-
+SELECT `tipus`, ROUND(AVG(`dij`), 2) AS 'Átlagos vitelidíj'
+FROM `hajo`
+GROUP BY `tipus`;
 -- 7. feladat
-
+SELECT `nev`, (`utas`*`dij`) AS 'Bevétel'
+FROM `hajo`;
 -- 8. feladat
-
+SELECT `hajoazon`, COUNT(`azon`) AS 'Bérelt'
+FROM `tura`
+GROUP BY `hajoazon`;
 -- 9. feladat
 
 -- 10. feladat
