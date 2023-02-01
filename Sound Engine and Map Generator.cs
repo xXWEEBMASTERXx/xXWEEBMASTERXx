e36a3d4a298d3bbaf53417ffbfdc6e680b73858e
xXWEEBMASTERXx <74237896+xXWEEBMASTERXx@users.noreply.github.com>
2023-02-01 12:14:41 +0100

    Create Sound Engine and Map Generator.cs

---
+++ Sound Engine and Map Generator.cs
@@ -0,0 +1,101 @@
+static void ConsoleDraw(IEnumerable<string> lines, int x, int y)
+        {
+            if (x > Console.WindowWidth) return;
+            if (y > Console.WindowHeight) return;
+
+            var trimLeft = x < 0 ? -x : 0;
+            int index = y;
+
+            x = x < 0 ? 0 : x;
+            y = y < 0 ? 0 : y;
+
+            var linesToPrint =
+                from line in lines
+                let currentIndex = index++
+                where currentIndex > 0 && currentIndex < Console.WindowHeight
+                select new
+                {
+                    Text = new String(line.Skip(trimLeft).Take(Math.Min(Console.WindowWidth - x, line.Length - trimLeft)).ToArray()),
+                    X = x,
+                    Y = y++
+                };
+
+            Console.Clear();
+            foreach (var line in linesToPrint)
+            {
+                Console.SetCursorPosition(line.X, line.Y);
+                Console.Write(line.Text);
+            }
+        }
+        class Sound
+        {
+            private MediaPlayer music_Player;
+
+            public void Play(string filename)
+            {
+                music_Player = new MediaPlayer();
+                music_Player.Open(new Uri(filename, UriKind.Relative));
+                music_Player.Play();
+            }
+            public void Stop()
+            {
+                music_Player.Stop();
+            }
+
+            public void SetVolume(int volume)
+            {
+                music_Player.Volume = volume / 100.0f;
+            }
+        }
+        const int MapWidth = 82;
+        const int MapHeight = 42;
+        const char Wall = '█';
+        const char Character = 'H';
+        private static void Write(int x, int y, char ch)
+        {
+            Console.SetCursorPosition(2 * x + 1, y + 1);
+            if (ch == Character)
+            {
+                Console.Write('╠'); Console.Write('╣');
+            }
+            else
+            {
+                Console.Write(ch); Console.Write(ch);
+            }
+
+        }
+
+        private static void DrawMap(char[,] map)
+        {
+            for (int x = 0; x < MapWidth; x++)
+            {
+                for (int y = 0; y < MapHeight; y++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Write(x, y, map[x, y]);
+                }
+            }
+        }
+
+        private static char[,] GenerateMap()
+        {
+            var map = new char[MapWidth, MapHeight];
+
+            for (int i = 0; i < MapWidth; i++)
+            {
+                map[i, 0] = Wall;
+            }
+            for (int i = 0; i < MapWidth; i++)
+            {
+                map[i, MapHeight - 1] = Wall;
+            }
+            for (int i = 0; i < MapHeight; i++)
+            {
+                map[0, i] = Wall;
+            }
+            for (int i = 0; i < MapHeight; i++)
+            {
+                map[MapWidth - 1, i] = Wall;
+            }
+            return map;
+        }
