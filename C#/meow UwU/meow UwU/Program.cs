cf92c028f8be2c3fade783e6c2d1c7bc666f0698
xXWEEBMASTERXx <74237896+xXWEEBMASTERXx@users.noreply.github.com>
2023-02-06 14:36:42 +0100

    C Octothorpe 2023. 02. 06.

---
+++ C#/meow UwU/meow UwU/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace meow_UwU
+{
+    class MyClass
+    {
+        double a;
+        double b;
+
+        public MyClass()
+        {
+            a = 1;
+            b = 2;
+        }
+        public MyClass(double aa, double bb)
+        {
+            a = aa;
+            b = bb;
+        }
+        public double A
+        {
+            get { return a; }
+            set {
+                if (value >= 0)
+                {
+                    a = value;
+                }
+            }
+        }
+        public double B
+        {
+            get { return b; }
+            set
+            {
+                if (value >= 0)
+                {
+                    b = value;
+                }
+            }
+        }
+        public void kerület()
+        {
+            Console.WriteLine(2*(a+b));
+        }
+        public void terület()
+        {
+            Console.WriteLine(a*b);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
