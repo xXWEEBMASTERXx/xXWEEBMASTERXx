ca00f850852ca354791607d742048cdab390167e
xXWEEBMASTERXx <74237896+xXWEEBMASTERXx@users.noreply.github.com>
2023-02-27 15:04:31 +0100

    C Octothorpe 2023. 02. 27.

---
+++ C#/elevator/elevator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Helló!");
+
+            Console.WriteLine("Add meg a neved! ");
+            string nev = Console.ReadLine();
+            Console.WriteLine("Szia"+nev+"!");
+
+            kettokerek k1 = new kettokerek();
+            Bringa b1 = new Bringa(1.8);
+
+            b1.Jelez();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/elevator/elevator/kettokerek.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elevator
+{
+    class kettokerek
+    {
+        protected double hossz;
+        public kettokerek()
+        {
+            hossz = 2;
+        }
+
+        public kettokerek(double h)
+        {
+            hossz = h;
+        }
+        public virtual void Jelez()
+        {
+
+
+        }
+    }
+
+    class Bringa : kettokerek
+    {
+        int maxSeb;
+        public Bringa(int km)
+        {
+            maxSeb = km;
+        }
+        
+        public Bringa(double h): base (h)
+        {
+            /*hossz = h;*/
+        }
+
+        public override void Jelez()
+        {
+            Console.WriteLine("Csengetek");
+        }
+    }
+
+}
