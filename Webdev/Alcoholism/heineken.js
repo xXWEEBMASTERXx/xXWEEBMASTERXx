12a5a0ac5d1ecc704752de3f3379ddf5bfe7bb80
xXWEEBMASTERXx <74237896+xXWEEBMASTERXx@users.noreply.github.com>
2023-04-03 13:49:40 +0200

    WebDev 2023. 04. 03.

--- Webdev/Alcoholism/heineken.js
+++ Webdev/Alcoholism/heineken.js
@@ -1,17 +1,71 @@
-document.write( "<table align=center id='szorzotabla'>" )
-
-for( i=1; i<=20; i++ )
-{
-    document.write( "<tr>" )
-
-    for( j=1; j<=20; j++ )
+function terfogat_szamol(mibol)
     {
-        if( i==1 || j==1)  document.write( "<td width='5%'             >" + i*j + "</td>" )
-        else               document.write( "<td title='" +i+ "*" +j+ "'>" + i*j + "</td>" )
+
+        pint  = parseFloat( document.terfogat.pint.value  )
+        liter = parseFloat( document.terfogat.liter.value )
+
+        if( mibol=="pintből"  && pint >0 )
+        {
+            document.terfogat.liter.value = pint  * 0.473
+        }
+
+        if( mibol=="literből" && liter>0 )
+        {
+            document.terfogat.pint.value  = liter / 0.473
+        }
+
     }
 
-    document.write( "</tr>" )
+function suly_szamol(mibol)
+    {
 
-}
+        uncia  = parseFloat( document.suly.uncia.value  )
+        gramm = parseFloat( document.suly.gramm.value )
 
-document.write( "</table>" )
+        if( mibol=="unciából"  && uncia >0 )
+        {
+            document.suly.gramm.value = uncia  * 28.35
+        }
+
+        if( mibol=="grammból" && gramm>0 )
+        {
+            document.suly.uncia.value  = gramm / 28.35
+        }
+
+    }
+
+function homerseklet_szamol(mibol)
+    {
+
+        Celsius  = parseFloat( document.homerseklet.Celsius.value  )
+        Fahrenheit = parseFloat( document.homerseklet.Fahrenheit.value ) 
+
+        if( mibol=="Celsiusból"  && Celsius >0 )
+        {
+            document.homerseklet.Fahrenheit.value = Math.round((Celsius * (9 / 5) ) + 32) 
+        }
+
+        if( mibol=="Fahrenheitből" && Fahrenheit>0 )
+        {
+            document.homerseklet.Celsius.value  = Math.round((Fahrenheit - 32) * (5 / 9))
+        }
+
+    }
+
+function tavolsag_szamol(mibol)
+    {
+
+        uncia  = parseFloat( document.suly.uncia.value  )
+        gramm = parseFloat( document.suly.gramm.value )
+
+        if( mibol=="hüvelykból"  && hüvelyk >0 )
+        {
+            document.suly.gramm.value = hüvelyk  * 28.35
+        }
+
+        if( mibol=="mmből" && mm>0 )
+        {
+            document.tavolsag.hüvelyk.value  = mm / 28.35
+        }
+
+    }
