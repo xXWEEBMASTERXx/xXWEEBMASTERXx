f7f067759e001b165542822918136a7d1e01fc21
xXWEEBMASTERXx <74237896+xXWEEBMASTERXx@users.noreply.github.com>
2023-04-26 10:52:33 +0200

    DataBase 2023. 04. 26.

---
+++ Szélerőművek/vezeteknev-keresztnev-szeleromuvek.sql
@@ -0,0 +1,56 @@
+-- 2. feladat
+CREATE DATABASE IF NOT EXISTS `szeleromuvek` {
+    CHARACTER SET = "útf8"
+    COLLATE = "utf8_hungarian_ci"
+}
+-- 3. feladat
+
+
+
+-- 5. feladat
+
+-- 6. feladat
+
+-- 7. feladat
+
+-- 8. feladat
+
+
+
+-- 10. feladat
+
+
+-- 11. feladat
+
+-- 12. feladat
+
+-- 13. feladat
+
+-- 14. feladat
+
+-- 15. feladat
+
+-- 16. feladat
+
+-- 17. feladat
+
+-- 18. feladat
+
+-- 19. feladat
+
+-- 20. feladat
+
+-- 21. feladat
+
+-- 22. feladat
+
+-- 23. feladat
+
+-- 24. feladat
+
+-- 25. feladat
+
+-- 26. feladat
+
+-- 27. feladat
+
