ff5e57b3b21059bef688b61a983049497ee3f66a
xXWEEBMASTERXx <74237896+xXWEEBMASTERXx@users.noreply.github.com>
2023-09-20 08:37:21 +0200

    hehe

---
+++ js go brrrrr/js.js
@@ -0,0 +1,25 @@
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+}
+function js_go_brrrrrr() {
+    while (true) {
+        console.log("Hey Everyone")
+        console.log("Hey Everyone")
+        console.log("Hey Everyone")
+        console.log("Hey Everyone")
+        console.log("Hey Everyone")
+        console.log("Hey Everyone")
+        console.log("Hey Everyone")
+        console.log("Hey Everyone")
+        console.log("Hey Everyone")
+        console.log("Hey Everyone")
+        console.log("Hey Everyone")
+        console.log("Hey Everyone")
+        console.log("Hey Everyone")
+    }
+}
